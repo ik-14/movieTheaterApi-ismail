7d424fbdef955ff79ff469b76b3ccc9c8472073c
ik14 <khalilismail616@gmail.com>
2022-11-11 15:26:36 +0000

    finished product

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,6 +5,7 @@
 
 const { Router } = require("express");
 const userRouter = Router();
+const db = require('../db')
 const {User, Show} = require('../models/index')
 
 userRouter.get('/', async(req,res) => {
@@ -15,8 +16,22 @@ userRouter.get('/:id', async(req,res) => {
   res.send( await User.findByPk(req.params.id))
 })
 
-userRouter.get('/:id/watched', async(req,res) => {
-  res.send
+
+userRouter.post('/pleaseWork', async(req,res) => {
+  const randomShow = await Show.create({
+    title: 'the greatest',
+    genre: 'nice genre bro',
+    rating: 2,
+    status: 'cancelled'
+  });
+  const randomUser = await User.create({
+    username: 'user1',
+    password: 'passwrdddd'
+  });
+  
+  randomUser.addShow(randomShow)
+  res.status(200).send('worked')
 })
+
 
 module.exports = userRouter
